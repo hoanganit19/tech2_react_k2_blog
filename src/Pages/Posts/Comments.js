b8a0ef2f8327133ac08257f462c5c6107e415099
Hoang An Unicode <hoanganit19@gmail.com>
2022-11-07 15:07:10 +0700

    Update code

--- src/Pages/Posts/Comments.js
+++ src/Pages/Posts/Comments.js
@@ -1,104 +1,127 @@
 import React, { Component } from "react";
+import withRouter from "../../Services/Routes/withRouter";
+import HttpClient from "../../Services/Helpers/Api/HttpClient";
+import moment from "moment";
+
+const md5 = require("md5");
+
+const client = new HttpClient();
 
 export class Comments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+    };
+  }
+
+  getComments = async (postId) => {
+    const res = await client.get(client.comments, {
+      postId: postId,
+      parentId: 0,
+    });
+    if (res.response.ok) {
+      const comments = res.data;
+      if (comments.length) {
+        for (const index in comments) {
+          const replyRes = await client.get(client.comments, {
+            postId: postId,
+            parentId: comments[index].id,
+          });
+          if (replyRes.response.ok && replyRes.data.length) {
+            comments[index].reply = replyRes.data;
+          }
+        }
+      }
+
+      this.setState({
+        comments: comments,
+      });
+    }
+  };
+
+  componentDidMount = () => {
+    const { id } = this.props.params;
+    this.getComments(id);
+  };
+
+  getGravatarURL = (email) => {
+    // Trim leading and trailing whitespace from
+    // an email address and force all characters
+    // to lower case
+    const address = String(email).trim().toLowerCase();
+
+    // Create an MD5 hash of the final string
+    const hash = md5(address);
+
+    // Grab the actual image URL
+    return `https://www.gravatar.com/avatar/${hash}`;
+  };
+
   render() {
+    const { comments } = this.state;
     return (
       <div className="comments">
-        <h5 className="comment-title py-4">2 Comments</h5>
-        <div className="comment d-flex mb-4">
-          <div className="flex-shrink-0">
-            <div className="avatar avatar-sm rounded-circle">
-              <img
-                className="avatar-img"
-                src="assets/img/person-5.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="flex-grow-1 ms-2 ms-sm-3">
-            <div className="comment-meta d-flex align-items-baseline">
-              <h6 className="me-2">Jordan Singer</h6>
-              <span className="text-muted">2d</span>
-            </div>
-            <div className="comment-body">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
-              minima ipsum at amet doloremque qui magni, placeat deserunt
-              pariatur itaque laudantium impedit aliquam eligendi repellendus
-              excepturi quibusdam nobis esse accusantium.
-            </div>
-            <div className="comment-replies bg-light p-3 mt-3 rounded">
-              <h6 className="comment-replies-title mb-4 text-muted text-uppercase">
-                2 replies
-              </h6>
-              <div className="reply d-flex mb-4">
+        <h5 className="comment-title py-4">{comments.length} Comments</h5>
+        {comments.length > 0 &&
+          comments.map(({ id, name, email, message, created_at, reply }) => {
+            return (
+              <div className="comment d-flex mb-3" key={id}>
                 <div className="flex-shrink-0">
                   <div className="avatar avatar-sm rounded-circle">
                     <img
                       className="avatar-img"
-                      src="assets/img/person-4.jpg"
+                      src={this.getGravatarURL(email)}
                       alt=""
                     />
                   </div>
                 </div>
-                <div className="flex-grow-1 ms-2 ms-sm-3">
-                  <div className="reply-meta d-flex align-items-baseline">
-                    <h6 className="mb-0 me-2">Brandon Smith</h6>
-                    <span className="text-muted">2d</span>
+                <div className="flex-shrink-1 ms-2 ms-sm-3">
+                  <div className="comment-meta d-flex">
+                    <h6 className="me-2">{name}</h6>
+                    <span className="text-muted">
+                      {moment(created_at).format("DD/MM/YYYY hh:mm:ss")}
+                    </span>
                   </div>
-                  <div className="reply-body">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  </div>
+                  <div className="comment-body">{message}</div>
+                  {reply?.length && (
+                    <div className="comment-replies bg-light p-3 mt-3 rounded">
+                      <h6 className="comment-replies-title mb-4 text-muted text-uppercase">
+                        {reply.length} replies
+                      </h6>
+                      {reply.map(({ id, name, email, message, created_at }) => {
+                        return (
+                          <div className="reply d-flex mb-4" key={id}>
+                            <div className="flex-shrink-0">
+                              <div className="avatar avatar-sm rounded-circle">
+                                <img
+                                  className="avatar-img"
+                                  src={this.getGravatarURL(email)}
+                                  alt=""
+                                />
+                              </div>
+                            </div>
+                            <div className="flex-grow-1 ms-2 ms-sm-3">
+                              <div className="reply-meta d-flex align-items-baseline">
+                                <h6 className="mb-0 me-2">{name}</h6>
+                                <span className="text-muted">{moment(created_at).format("DD/MM/YYYY hh:mm:ss")}</span>
+                              </div>
+                              <div className="reply-body">
+                                {message}
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  )}
                 </div>
               </div>
-              <div className="reply d-flex">
-                <div className="flex-shrink-0">
-                  <div className="avatar avatar-sm rounded-circle">
-                    <img
-                      className="avatar-img"
-                      src="assets/img/person-3.jpg"
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className="flex-grow-1 ms-2 ms-sm-3">
-                  <div className="reply-meta d-flex align-items-baseline">
-                    <h6 className="mb-0 me-2">James Parsons</h6>
-                    <span className="text-muted">1d</span>
-                  </div>
-                  <div className="reply-body">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Distinctio dolore sed eos sapiente, praesentium.
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="comment d-flex">
-          <div className="flex-shrink-0">
-            <div className="avatar avatar-sm rounded-circle">
-              <img
-                className="avatar-img"
-                src="assets/img/person-2.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="flex-shrink-1 ms-2 ms-sm-3">
-            <div className="comment-meta d-flex">
-              <h6 className="me-2">Santiago Roberts</h6>
-              <span className="text-muted">4d</span>
-            </div>
-            <div className="comment-body">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-              laborum in corrupti dolorum, quas delectus nobis porro accusantium
-              molestias sequi.
-            </div>
-          </div>
-        </div>
+            );
+          })}
       </div>
     );
   }
 }
 
-export default Comments;
+export default withRouter(Comments);
